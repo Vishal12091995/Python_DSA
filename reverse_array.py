e20e895340e8963611d59439415fc23202158baf
Vishal Gupta <117042853+Vishal12091995@users.noreply.github.com>
2024-08-07 17:12:08 +0530

    Add files via upload

--- reverse_array.py
+++ reverse_array.py
@@ -1,4 +1,12 @@
 # Reverse  an array in provided list
 list=[a for a in input("enter number with space").split(" ")]
-print(list[::-1])
+# print(list[::-1])
 
+# by using function
+
+def reverse_list(list):
+    list.reverse()
+    return " ".join(list)
+
+
+print(reverse_list(list))
